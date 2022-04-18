ff8d5f8910317c3c2d39871d7794f3a9fc53bf72
Adam C <acc1729@gmail.com>
2022-04-18 18:15:17 -0400

    Fix a lot of janky design decisions in board.zig

--- src/board.zig
+++ src/board.zig
@@ -26,39 +26,48 @@ pub const Location = struct {
 };
 
 // Viewport for game board will be 60 by 25, but that'll include borders.
-const WIDTH = 60 - 2;
-const HEIGHT = 25 - 2;
+const WIDTH = 8;
+const HEIGHT = 4;
 
-pub const Map = struct {
-    tiles: *[25][80]Location,
+pub const Board = [HEIGHT][WIDTH]Location;
+const BoardRepr = [HEIGHT][WIDTH]u8;
 
-    const Self = @This();
+pub fn init(alloc: std.mem.Allocator) !*Board {
+    var board = try alloc.create(Board);
+    var h: u8 = 1;
+    var w: u8 = 0;
+    while (w < WIDTH) : (w += 1) {
+        board[0][w] = Location{ .kind = Tile.wall };
 
-    pub fn init(alloc: std.mem.Allocator) !Self {
-        var board = try alloc.create([25][80]Location);
-        var h: u8 = 1;
-        var w: u8 = 0;
-        while (w < WIDTH) : (w += 1) {
-            board[0][w] = Location{ .kind = Tile.wall };
-
-            board[HEIGHT][w] = Location{ .kind = Tile.wall };
-        }
-        while (h < HEIGHT - 1) : (h += 1) {
-            board[h][0] = Location{ .kind = Tile.wall };
-            w = 1;
-            while (w < WIDTH - 1) : (w += 1) {
-                board[h][w] = Location{ .kind = Tile.floor };
-            }
-
-            board[h][0] = Location{ .kind = Tile.wall };
-        }
-        return Self{.tiles = board};
+        board[HEIGHT - 1][w] = Location{ .kind = Tile.wall };
     }
-    
-    pub fn deinit(self: *Self, alloc: std.mem.Allocator) void {
-        alloc.free(self.tiles);
+    while (h < HEIGHT - 1) : (h += 1) {
+        board[h][0] = Location{ .kind = Tile.wall };
+        w = 1;
+        while (w < WIDTH - 1) : (w += 1) {
+            board[h][w] = Location{ .kind = Tile.floor };
+        }
+
+        board[h][WIDTH - 1] = Location{ .kind = Tile.wall };
     }
-};
+    return board;
+}
+
+pub fn deinit(board: *Board, alloc: std.mem.Allocator) void {
+    alloc.free(board.tiles);
+}
+
+pub fn toString(board: *Board, alloc: std.mem.Allocator) !*BoardRepr {
+    var repr = try alloc.create(BoardRepr);
+    for (board) |row, h| {
+        for (row) |tile, w| {
+            repr[h][w] = tile.kind.repr();
+        }
+    }
+    return repr;
+}
+
+const ta = std.testing.allocator;
 
 test "Tile types have representation." {
     const floor = Tile.floor;
@@ -68,8 +77,21 @@ test "Tile types have representation." {
 }
 
 test "Map is populated." {
-    const ta = std.testing.allocator;
-    const board = try Map.init(ta);
+    const board = try init(ta);
     defer ta.destroy(board);
-    try std.testing.expect(board.tiles[0][0].kind == Tile.wall);
+    try std.testing.expect(board[0][0].kind == Tile.wall);
+    try std.testing.expect(board[1][1].kind == Tile.floor);
+    try std.testing.expect(board[2][1].kind == Tile.floor);
+    try std.testing.expect(board[1][WIDTH - 1].kind == Tile.wall);
+    try std.testing.expect(board[HEIGHT - 1][WIDTH - 1].kind == Tile.wall);
+}
+
+test "toString" {
+    const board = try init(ta);
+    defer ta.destroy(board);
+    const repr = try toString(board, ta);
+    defer ta.destroy(repr);
+    try std.testing.expectEqual(repr[0][0], '#');
+    try std.testing.expectEqual(repr[1][1], '.');
+    try std.testing.expectEqual(repr[HEIGHT - 1][WIDTH - 1], '#');
 }
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,21 @@
 const std = @import("std");
 
+const board = @import("board.zig");
+
+const NEWLINE: [1]u8 = .{'\n'};
+
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    const map = try board.init(alloc);
+    defer alloc.destroy(map);
+    const stdout = std.io.getStdOut();
+    const repr = try board.toString(map, alloc);
+    defer alloc.destroy(repr);
+    for (repr) |*line| {
+        _ = try stdout.write(line);
+        _ = try stdout.write(&NEWLINE);
+    }
 }
 
 test "basic test" {
